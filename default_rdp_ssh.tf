875d68778985e956a5f550ac52b11eac1748ebc1
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-05-29 13:55:08 +0300

    Add files via upload

---
+++ default_rdp_ssh.tf
@@ -0,0 +1,12 @@
+ resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.mainvpc.id
+
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 3389
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  }
