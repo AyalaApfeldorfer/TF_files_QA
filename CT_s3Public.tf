bdf650bf8471a513662b967e36172086c7112c24
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-05-30 13:52:12 +0300

    Update CT_s3Public.tf

--- CT_s3Public.tf
+++ CT_s3Public.tf
@@ -1,10 +1,12 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "example"
+resource "aws_cloudtrail" "my_aws_cloudtrail_connected_to_bucket_publicRead_failed" {
+  name                          = "my_aws_cloudtrail"
+  s3_bucket_name                =  aws_s3_bucket.my_aws_s3_bucket_publicRead
 }
 
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.example.id
 
-  block_public_acls   = true
-  block_public_policy = true
+  block_public_acls   = false
+  block_public_policy = false
 }
+
