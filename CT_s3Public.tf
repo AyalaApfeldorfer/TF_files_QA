e4c807e74006364b12591416252c0ea039720956
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-05-30 16:11:01 +0300

    Delete CT_s3Public.tf

--- CT_s3Public.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_cloudtrail" "my_aws_cloudtrail_connected_to_bucket_publicRead_failed" {
-  name                          = "my_aws_cloudtrail"
-  s3_bucket_name                = aws_s3_bucket.my_aws_s3_bucket_publicRead
-}
-resource "aws_s3_bucket" "my_aws_s3_bucket_publicRead" {
-  acl = "public-read"
-}
