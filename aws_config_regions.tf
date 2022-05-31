7c5640f33936df09ed0e7d73977a72f55e17430b
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-05-31 10:41:31 +0300

    Update aws_config_regions.tf

--- aws_config_regions.tf
+++ aws_config_regions.tf
@@ -1,11 +1,7 @@
 resource "aws_cloudtrail" "my_aws_cloudtrail_connected_to_bucket_publicRead_failed" {
   name                          = "my_aws_cloudtrail"
-  s3_bucket_name                =  aws_s3_bucket.my_aws_s3_bucket_publicRead
+  s3_bucket_name                = aws_s3_bucket.my_aws_s3_bucket_publicRead
 }
-
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
-
-  block_public_acls   = false
-  block_public_policy = false
+resource "aws_s3_bucket" "my_aws_s3_bucket_publicRead" {
+  acl = "public-read"
 }
