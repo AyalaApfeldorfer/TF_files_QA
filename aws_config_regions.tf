70f6ca46214621e1b8d627909cd56bbcbaf344e8
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-05-31 09:22:48 +0300

    Update aws_config_regions.tf

--- aws_config_regions.tf
+++ aws_config_regions.tf
@@ -1,6 +1,24 @@
-module "aws_config" {
-  source = "trussworks/config/aws"
+resource "aws_config_configuration_recorder" "foo" {
+  name     = "example"
+  role_arn = aws_iam_role.r.arn
+}
 
-  config_name        = "my-aws-config"
-  config_logs_bucket = "my-aws-logs"
+resource "aws_iam_role" "r" {
+  name = "awsconfig-example"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "config.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
 }
