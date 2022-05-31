469ff399fd5c8cc120e6f8d907c30cbef51bcb98
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-05-31 10:36:45 +0300

    Update aws_config_regions.tf

--- aws_config_regions.tf
+++ aws_config_regions.tf
@@ -1,24 +1,11 @@
-resource "aws_config_configuration_recorder" "foo" {
-  name     = "example"
-  role_arn = aws_iam_role.r.arn
+resource "aws_cloudtrail" "my_aws_cloudtrail_connected_to_bucket_publicRead_failed" {
+  name                          = "my_aws_cloudtrail"
+  s3_bucket_name                =  aws_s3_bucket.my_aws_s3_bucket_publicRead
 }
 
-resource "aws_iam_role" "r" {
-  name = "awsconfig-example"
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "config.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-POLICY
+  block_public_acls   = false
+  block_public_policy = false
 }
