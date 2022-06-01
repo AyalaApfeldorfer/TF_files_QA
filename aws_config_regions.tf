bc67a36868b4ce1b01aff9a97aeb4e08b4348377
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-01 14:58:33 +0300

    Update aws_config_regions.tf

--- aws_config_regions.tf
+++ aws_config_regions.tf
@@ -1,21 +1,3 @@
-resource "aws_cloudtrail" "cloudtrail1_passed" {
-  name                          = "cloudtrail1_passed"
-  s3_bucket_name                = aws_s3_bucket.foo.id
-  s3_key_prefix                 = "prefix"
-  include_global_service_events = false
-  cloud_watch_logs_group_arn = aws_cloudwatch_log_group.log_group1.arn
-  is_multi_region_trail = true
-}
-
-resource "aws_cloudwatch_log_group" "log_group1" {
-  name = "log_group1"
-
-  tags = {
-    Environment = "production"
-    Application = "serviceA"
-  }
-}
-
 resource "aws_iam_group" "group" {
   name = "test-group"
 }
