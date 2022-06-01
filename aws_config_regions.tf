9ee4598f8a815913af62b72fdb3db9b83c17978d
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-01 15:10:46 +0300

    Update aws_config_regions.tf

--- aws_config_regions.tf
+++ aws_config_regions.tf
@@ -1,9 +1,3 @@
 resource "aws_iam_role" "role" {
   name = "test-role"
 }
-
-resource "aws_iam_policy_attachment" "test-attach" {
-  name       = "test-attachment"
-  roles      = [aws_iam_role.role.name]
-  policy_arn = aws_iam_policy.policy.arn
-}
