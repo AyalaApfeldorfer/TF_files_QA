867e3a82525c856dd9cd107540116890380aed2d
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-01 16:44:28 +0300

    Update aws_config_regions.tf

--- aws_config_regions.tf
+++ aws_config_regions.tf
@@ -7,7 +7,7 @@ resource "aws_cloudtrail" "cloudtrail1_passed" {
   name                          = "cloudtrail1_passed"
   s3_bucket_name                = aws_s3_bucket.foo.id
   s3_key_prefix                 = "prefix"
-  include_global_service_events = false
+  include_global_service_events = true
   cloud_watch_logs_group_arn = aws_cloudwatch_log_group.log_group1.arn
   is_multi_region_trail = true
 }
