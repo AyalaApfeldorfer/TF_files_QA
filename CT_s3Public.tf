006094725ebca729590964fa5088584b81e138ec
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-02 11:35:50 +0300

    Update CT_s3Public.tf

--- CT_s3Public.tf
+++ CT_s3Public.tf
@@ -1,7 +1,49 @@
-resource "aws_cloudtrail" "my_aws_cloudtrail_connected_to_bucket_publicRead_failed" {
-  name                          = "my_aws_cloudtrail"
-  s3_bucket_name                = aws_s3_bucket.my_aws_s3_bucket_publicRead
+resource "aws_iam_user" "user" {
+  name = "test-user"
 }
-resource "aws_s3_bucket" "my_aws_s3_bucket_publicRead" {
-  acl = "public-read"
+resource "aws_iam_role" "role" {
+  name = "test-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_group" "group" {
+  name = "test-group"
+}
+resource "aws_iam_policy" "policy" {
+  name        = "AWSSupportAccess"
+  description = "A test policy"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  users      = [aws_iam_user.user.name]
+  roles      = [aws_iam_role.role.name]
+  groups     = [aws_iam_group.group.name]
+  policy_arn = aws_iam_policy.policy.arn
 }
