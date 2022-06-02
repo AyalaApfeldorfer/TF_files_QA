105eb1ab70dc8e4b32d7fc98855acebc30c5cdb3
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-02 12:04:41 +0300

    Update Cloudtrail (1).tf

--- Cloudtrail (1).tf
+++ Cloudtrail (1).tf
@@ -5,13 +5,20 @@ provider "aws" {
 
 resource "aws_cloudtrail" "cloudtrail_single_region_failed" {
   name                          = "cloudtrail_single_region_failed"
+  s3_bucket_name                = aws_s3_bucket.foo.id
+  s3_key_prefix                 = "prefix"
   include_global_service_events = false
   cloud_watch_logs_group_arn = aws_cloudwatch_log_group.log_group1.arn
-  is_multi_region_trail = flase
+  is_multi_region_trail = false
 }
 
 resource "aws_cloudwatch_log_group" "log_group1" {
   name = "log_group1"
+
+  tags = {
+    Environment = "production"
+    Application = "serviceA"
+  }
 }
 
 resource "aws_cloudwatch_log_metric_filter" "metric_filter1" {
