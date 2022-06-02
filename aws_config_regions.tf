c9bcdf7933b9ec3b7028d2a111c1da257a52b0a2
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-02 11:02:29 +0300

    Update aws_config_regions.tf

--- aws_config_regions.tf
+++ aws_config_regions.tf
@@ -3,12 +3,38 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-
-resource "aws_cloudtrail" "cloudtrail_single_region_failed" {
-  name                          = "cloudtrail_single_region_failed"
+resource "aws_cloudtrail" "cloudtrail1_passed" {
+  name                          = "cloudtrail1_passed"
   s3_bucket_name                = aws_s3_bucket.foo.id
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
   cloud_watch_logs_group_arn = aws_cloudwatch_log_group.log_group1.arn
-  is_multi_region_trail = flase
+  is_multi_region_trail = true
+}
+
+resource "aws_cloudwatch_log_group" "log_group1" {
+  name = "log_group1"
+
+  tags = {
+    Environment = "production"
+    Application = "serviceA"
+  }
+}
+
+resource "aws_cloudwatch_log_metric_filter" "metric_filter1" {
+  name           = "metric_filter1"
+  pattern        = "{ ($.eventName = CreateTrail) || ($.eventName = UpdateTrail) || ($.eventName = DeleteTrail) || ($.eventName = StartLogging) || ($.eventName = StopLogging) }"
+  log_group_name = aws_cloudwatch_log_group.log_group1
+
+  metric_transformation {
+    name      = "EventCount"
+    namespace = "YourNamespace"
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "alarm_sns_actions_enabled" {
+  metric_name = aws_cloudwatch_log_metric_filter.metric_filter1.name
+  actions_enabled = true
+  alarm_actions       = [aws_autoscaling_policy.bat.arn,aws_sns_topic.sns.arn]
 }
