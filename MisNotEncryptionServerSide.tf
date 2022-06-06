24639e743dcd0054fb01ee19f568bd689fdd4cc9
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-06 17:25:26 +0300

    Update MisNotEncryptionServerSide.tf

--- MisNotEncryptionServerSide.tf
+++ MisNotEncryptionServerSide.tf
@@ -1,7 +1,7 @@
+resource "aws_cloudtrail" "my_aws_cloudtrail_log" {
+  name = "my_aws_cloudtrail"
+  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.example.arn}:*"
+}
 resource "aws_cloudwatch_log_group" "example" {
   name = "Example"
-}
-
-resource "aws_cloudtrail" "example" {
-  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.example.arn}:*"
 }
