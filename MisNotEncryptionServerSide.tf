9e8f277d6d140e98b3f4abe5ae733ed1f49d9e7c
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-06 14:34:48 +0300

    Update MisNotEncryptionServerSide.tf

--- MisNotEncryptionServerSide.tf
+++ MisNotEncryptionServerSide.tf
@@ -7,7 +7,7 @@ module "s3-bucket_example_server-side-encryption" {
 resource "aws_kms_key" "mykey" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 10
-  enable_key_rotation = false
+  enable_key_rotation = true
 }
 
 resource "aws_s3_bucket" "mybucket" {
