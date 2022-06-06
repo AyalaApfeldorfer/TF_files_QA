8aeafb8105413148ae10e4553b80abac87fd1c4f
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-06 15:40:16 +0300

    Update MisNotEncryptionServerSide.tf

--- MisNotEncryptionServerSide.tf
+++ MisNotEncryptionServerSide.tf
@@ -1,7 +1,6 @@
 resource "aws_cloudtrail" "my_aws_cloudtrail_connected_to_bucket_publicRead_failed" {
   name                          = "my_aws_cloudtrail"
   s3_bucket_name                = aws_s3_bucket.my_aws_s3_bucket_publicRead
-  cloudwatch_log_group_name     = aws_cloudwatch_log_group.yada
 }
 
 resource "aws_s3_bucket" "my_aws_s3_bucket_publicRead" {
