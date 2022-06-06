1815390bef20af4aaa34373bd39ae132d5af1d21
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-06 17:05:20 +0300

    Update MisNotEncryptionServerSide.tf

--- MisNotEncryptionServerSide.tf
+++ MisNotEncryptionServerSide.tf
@@ -1,9 +1,12 @@
-
-resource "aws_cloudwatch_log_group" "yada" {
-  name = "Yada"
-
-  tags = {
-    Environment = "production"
-    Application = "serviceA"
-  }
+resource "aws_cloudtrail" "my_aws_cloudtrail_connected_to_bucket_publicRead_failed" {
+  name                          = "my_aws_cloudtrail"
+  s3_bucket_name                = aws_s3_bucket.my_aws_s3_bucket_publicRead
+  kms_key_name                  = aws_kms_key.a
+}
+resource "aws_s3_bucket" "my_aws_s3_bucket_publicRead" {
+  acl = "public-read"
+}
+resource "aws_kms_key" "a" {
+  description             = "KMS key 1"
+  deletion_window_in_days = 10
 }
