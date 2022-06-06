1653db95fded02b0a25b51648f8f009426ca29bd
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-06 11:24:17 +0300

    Update Cloudtrail (1).tf

--- Cloudtrail (1).tf
+++ Cloudtrail (1).tf
@@ -1,30 +1,20 @@
-resource "aws_iam_user" "user" {
-  name = "test-user"
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
 }
-resource "aws_iam_role" "role" {
-  name = "test-role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
+resource "aws_iam_user" "user_with_internal_policy" {
+  name = "user_with_internal_policy"
+  permissions_boundary = aws_iam_policy.internal_policy_with_user.arn
 }
-EOF
+resource "aws_iam_user" "user_with_managed_policy" {
+  name = "user_with_managed_policy"
 }
-resource "aws_iam_group" "group" {
-  name = "test-group"
+resource "aws_iam_role" "role_with_managed_policy" {
+  name = "role_with_managed_policy"
 }
-resource "aws_iam_policy" "policy" {
-  name        = "AWSSupportAccess"
-  description = "A test policy"
+resource "aws_iam_policy" "policy_no_user" {
+  name        = "policy_no_user"
+  description = "Policy not attached to users"
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -40,9 +30,67 @@ resource "aws_iam_policy" "policy" {
 }
 EOF
 }
-resource "aws_iam_policy_attachment" "test-attach" {
-  name       = "AWSSupportAccess"
-  users      = [aws_iam_user.user.name]
-  roles      = [aws_iam_role.role.name]
-  groups     = [aws_iam_group.group.name]
+resource "aws_iam_policy" "internal_policy_with_user" {
+  name        = "internal_policy_with_user"
+  description = "An internal policy attached to user"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_policy" "managed_policy_with_user" {
+  name        = "managed_policy_with_user"
+  description = "A managed policy attached to user"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_policy" "managed_policy_with_role" {
+  name        = "managed_policy_with_role"
+  description = "A managed policy attached to role"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_policy_attachment" "attach_user" {
+  name       = "managed_policy_attachment_to_user"
+  users      = [aws_iam_user.user_with_managed_policy.name]
+  policy_arn = aws_iam_policy.managed_policy_with_user.arn
+}
+resource "aws_iam_policy_attachment" "attach_role" {
+  name       = "managed_policy_attachment_to_role"
+  roles      = [aws_iam_user.role_with_managed_policy.name]
+  policy_arn = aws_iam_policy.managed_policy_with_role.arn
 }
