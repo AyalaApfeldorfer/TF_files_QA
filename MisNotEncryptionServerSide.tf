af660976d62ece416ae39a2c1cd0b60ae0a8a250
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-07 11:51:09 +0300

    Update MisNotEncryptionServerSide.tf

--- MisNotEncryptionServerSide.tf
+++ MisNotEncryptionServerSide.tf
@@ -22,10 +22,7 @@ resource "aws_config_delivery_channel" "foo" {
 resource "aws_config_configuration_recorder" "foo" {
   name     = "example"
   role_arn = aws_iam_role.r.arn
-  recording_group {
-    all_supported                 = "false"
-    }
-}
+  }
 
 resource "aws_iam_role" "r" {
   name = "example-awsconfig"
