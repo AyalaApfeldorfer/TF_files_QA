0cb461f5a277a697ddcf5c12a778de5a883b2689
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-07 16:00:34 +0300

    Update CT_LogValidationTrue.tf

--- CT_LogValidationTrue.tf
+++ CT_LogValidationTrue.tf
@@ -1,7 +1,27 @@
-resource "aws_cloudtrail" "my_aws_cloudtrail_connected_to_bucket_publicRead_failed" {
-  name                          = "my_aws_cloudtrail"
-  s3_bucket_name                = aws_s3_bucket.my_aws_s3_bucket_publicRead
+resource "aws_iam_account_password_policy" "noAllow" {
+  minimum_password_length        = 9
+  require_lowercase_characters   = false
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  password_reuse_prevention = 3
+  max_password_age = 12
 }
-resource "aws_s3_bucket" "my_aws_s3_bucket_publicRead" {
-  acl = "public-read-write"
+resource "aws_iam_account_password_policy" "noReuse" {
+  minimum_password_length        = 6
+  require_lowercase_characters   = true
+  require_numbers                = false
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = false
+  max_password_age = 0
 }
+resource "aws_iam_account_password_policy" "noMax" {
+  minimum_password_length        = 6
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = false
+  allow_users_to_change_password = false
+  password_reuse_prevention = 3
+  }
