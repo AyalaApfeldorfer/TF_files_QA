f365f29670f2ede428e28f81959f47978cdb827a
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-07 13:07:23 +0300

    Update MisNotEncryptionServerSide.tf

--- MisNotEncryptionServerSide.tf
+++ MisNotEncryptionServerSide.tf
@@ -1,6 +1,6 @@
 resource "aws_config_configuration_recorder_status" "foo" {
   name       = aws_config_configuration_recorder.foo.name
-  is_enabled = false
+  is_enabled = true
   depends_on = [aws_config_delivery_channel.foo]
   last_status = "SUCCESS"
 }
@@ -22,10 +22,7 @@ resource "aws_config_delivery_channel" "foo" {
 resource "aws_config_configuration_recorder" "foo" {
   name     = "example"
   role_arn = aws_iam_role.r.arn
-  recording_group {
-    include_global_resource_types                = "true"
-    }
-}
+ }
 
 resource "aws_iam_role" "r" {
   name = "example-awsconfig"
