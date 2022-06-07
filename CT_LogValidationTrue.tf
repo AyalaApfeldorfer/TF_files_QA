608af39ece9309399a75708a30f7d4fb9a28d3e5
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-07 16:32:33 +0300

    Update CT_LogValidationTrue.tf

--- CT_LogValidationTrue.tf
+++ CT_LogValidationTrue.tf
@@ -1,9 +1,9 @@
 resource "aws_iam_account_password_policy" "noAllow" {
-  minimum_password_length        = 9
+  minimum_password_length        = 15
   require_lowercase_characters   = true
   require_numbers                = true
   require_uppercase_characters   = true
   require_symbols                = true
-  password_reuse_prevention = 24
-  max_password_age = 90
+  password_reuse_prevention = 25
+  max_password_age = 95
 }
