3b2b99917e04f37ca2a06ae489177f22a458b188
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-07 10:34:32 +0300

    Update MisNotEncryptionServerSide.tf

--- MisNotEncryptionServerSide.tf
+++ MisNotEncryptionServerSide.tf
@@ -1,7 +1,24 @@
-resource "aws_cloudwatch_log_group" "example" {
-  name = "Example"
+resource "aws_config_configuration_recorder" "foo" {
+  name     = "example"
+  role_arn = aws_iam_role.r.arn
 }
-resource "aws_cloudtrail" "example" {
-  name = "my_aws_cloudtrail"
-  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.example.arn}:*"
+
+resource "aws_iam_role" "r" {
+  name = "awsconfig-example"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "config.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
 }
