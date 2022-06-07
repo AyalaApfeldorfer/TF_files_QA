a0253d24d3af22e30b51222a1f769e75e15e60dd
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-07 15:37:34 +0300

    Update CT_LogValidationTrue.tf

--- CT_LogValidationTrue.tf
+++ CT_LogValidationTrue.tf
@@ -3,5 +3,5 @@ resource "aws_cloudtrail" "my_aws_cloudtrail_connected_to_bucket_publicRead_fail
   s3_bucket_name                = aws_s3_bucket.my_aws_s3_bucket_publicRead
 }
 resource "aws_s3_bucket" "my_aws_s3_bucket_publicRead" {
-  acl = "public-write"
+  acl = "public-read-write"
 }
