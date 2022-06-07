3045ebed7ebdf8f6be944b0d2319f878ca381d0c
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-07 14:56:10 +0300

    Update CT_LogValidationTrue.tf

--- CT_LogValidationTrue.tf
+++ CT_LogValidationTrue.tf
@@ -9,5 +9,15 @@ resource "aws_s3_bucket" "b" {
 
 resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.b.id
-  acl    = "public"
+  grant {
+    id          = data.aws_canonical_user_id.current_user.id
+    type        = "CanonicalUser"
+    permissions = ["FULL_CONTROL"]
+  }
+
+  grant {
+    type        = "Group"
+    permissions = ["READ_ACP", "WRITE"]
+    uri         = "http://acs.amazonaws.com/groups/s3/LogDelivery"
+  }
 }
