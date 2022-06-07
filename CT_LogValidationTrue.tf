5422164c0c2b9f65cf1baa70a3647719026f17f4
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-07 12:19:35 +0300

    Update CT_LogValidationTrue.tf

--- CT_LogValidationTrue.tf
+++ CT_LogValidationTrue.tf
@@ -3,6 +3,7 @@ resource "aws_cloudtrail" "foobar" {
   s3_bucket_name                = aws_s3_bucket.foo.id
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
-enable_log_file_validation = true
-is_multi_region_trail = true
-}
+  enable_log_file_validation = true
+  is_multi_region_trail = true
+  enable_logging = true 
+ }
