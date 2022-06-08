30597ccb2c829111076c2b3fe15f81a16b7cf371
AyalaApfeldorfer <104299002+AyalaApfeldorfer@users.noreply.github.com>
2022-06-08 13:42:19 +0300

    Update test_terra.tf

--- test_terra.tf
+++ test_terra.tf
@@ -15,7 +15,7 @@ resource "aws_cloudwatch_log_group" "log_group1" {
 }
 resource "aws_cloudwatch_log_metric_filter" "metric_filter1" {
   name           = "metric_filter1"
-  pattern        = "{ ($.eventName = CreateTrail) || ($.eventName = UpdateTrail) || ($.eventName = DeleteTrail) || ($.eventName = StartLogging) || ($.eventName = StopLogging) }"
+  pattern        = "{ ($.eventSource = kms.amazonaws.com) && (($.eventName=DisableKey) || ($.eventName=ScheduleKeyDeletion)) }"
   log_group_name = aws_cloudwatch_log_group.log_group1
   metric_transformation {
     name      = "EventCount"
